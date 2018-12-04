73bbabd7737e8476356d6832e4760a89f46b1567
Valentin Meica <valentinmeica@gmail.com>
2018-12-04 18:24:20 -0500

    initial setup

---
+++ liri.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+//api keys
+const spotify = new Spotify(keys.spotify);
+
+//list of comands that Liri will take in
+
+//concert-this
+//spotify-this-song
+//movie-this
+//do-what-it-says
